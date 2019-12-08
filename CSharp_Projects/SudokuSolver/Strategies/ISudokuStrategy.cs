6e7a6dd5f1103ee22e3453a03a4a64eb91b705c8
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-08 13:18:36 +0200

    Sudoku Solver Project Part 3 - Engine and Simple Mark Up Strategy Classes implementation

---
+++ CSharp_Projects/SudokuSolver/Strategies/ISudokuStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.Strategies
+{
+    public interface ISudokuStrategy
+    {
+        int[,] Solve(int[,] sudokuBoard);
+    }
+}
